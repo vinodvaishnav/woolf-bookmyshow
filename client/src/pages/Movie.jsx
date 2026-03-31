2ad1743db9855f4aad693f420fb0fa53d4d02667
Vinod Vaishnav <vaishnav.vinod484@gmail.com>
2026-04-01 00:54:25 +0530

    [client]: Movie detail page update

--- client/src/pages/Movie.jsx
+++ client/src/pages/Movie.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { Image } from 'antd';
 
 import { getMovieDetail } from "../redux/movieSlice";
 
@@ -16,12 +17,32 @@ const Movie = () => {
 
     }, [movieId]);
 
-    return loading ? <div className='movie-container'>Loading...</div> : <div className='movie-container'>
-        Movie Detail: {movieDetail ? <div>
-            <h1>{movieDetail.name}</h1>
-            <img src={movieDetail.poster} alt={movieDetail.name} />
-        </div> : 'No Movie Found'}
-    </div>
+    return loading ?
+        <div className='movie-container'>Loading...</div> :
+        <div className='movie-container'>
+            {
+                movieDetail ?
+                    <div className='movie-detail'>
+                        <div className="info" style={{ backgroundImage: `url(${movieDetail.poster})` }}>
+                            <Image
+                                width={200}
+                                alt={movieDetail.name}
+                                src={movieDetail.thumbnail}
+                                preview={false}
+                            >
+                            </Image>
+                            <div>
+                                <h1>{movieDetail.name}</h1>
+                            </div>
+                        </div>
+                        <div className="movie-detail2">
+                            <h2>About Movie</h2>
+                            <p>{movieDetail.description}</p>
+                        </div>
+                    </div>
+                    : 'No Movie Found'
+            }
+        </div >
 }
 
 export default Movie;
