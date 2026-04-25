bd33647e8cd3a0225a4780bf1f9f8d0cb856e206
Vinod Vaishnav <vaishnav.vinod484@gmail.com>
2026-04-25 12:51:32 +0530

    [client]: Fixed Login flow

--- client/src/util/api_client.js
+++ client/src/util/api_client.js
@@ -15,7 +15,14 @@ const API_BASE_URL = 'http://localhost:5500/api';
 // export default apiClient;
 
 const getApiClient = () => {
-    localStorage.getItem('token') && (axios.defaults.headers.common['authorization'] = `Bearer ${localStorage.getItem('token')}`);
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        axios.defaults.headers.common['authorization'] = `Bearer ${token}`;
+    } else {
+        delete axios.defaults.headers.common['authorization'];
+    }
+
     axios.defaults.baseURL = API_BASE_URL;
     axios.defaults.timeout = 3000;
 
